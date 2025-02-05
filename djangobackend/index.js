4ab2daaf6ea0f8e76b87bb21a2f0b6e667581b25
Henrique Salgueiro <henriquesalgueiro.dev@gmail.com>
2025-02-05 15:09:13 -0300

    Try running Django server

--- djangobackend/index.js
+++ djangobackend/index.js
@@ -1,11 +1,13 @@
 const functions = require("firebase-functions");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-const apiProxy = createProxyMiddleware({
-  target: "https://portal-jonas-madureira.web.app", // URL do seu servidor Django
-  changeOrigin: true,
-});
+const { exec } = require("child_process");
 
 exports.djangoBackend = functions.https.onRequest((req, res) => {
-  apiProxy(req, res);
+  exec("python manage.py runserver 0.0.0.0:8000", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.status(500).send(`Server error: ${error}`);
+      return;
+    }
+    res.status(200).send(stdout);
+  });
 });
